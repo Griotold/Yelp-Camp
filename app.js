c2b78d52b48a9479df17521231c7aa498c62db1f
Griotold <gotjd9773@naver.com>
2023-01-19 10:35:01 +0900

    campground Index

--- app.js
+++ app.js
@@ -15,11 +15,7 @@ db.once("open", () => {
     console.log("Database connected");
 })
     
-
-
 const app = express();
-
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -27,10 +23,9 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!' });
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
 })
 
 app.listen(3000, () => {
