96b4a800a4acfcb0ff5871eed618d161aad61d41
Griotold <gotjd9773@naver.com>
2023-01-19 11:02:05 +0900

    campground New & Create

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -26,6 +28,17 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
+})
+// New -> Show 보다 위에 있어야 함.
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+// Create
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 
 // Show
