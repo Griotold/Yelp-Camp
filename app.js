415d2563c9f72cefead9d4d597fa94a7186e5ff5
Griotold <gotjd9773@naver.com>
2023-01-19 11:49:12 +0900

    campground Edit & Upgrade

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const uri = 'mongodb://127.0.0.1:27017/yelp-camp';
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect(uri, {
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -45,6 +47,19 @@ app.post('/campgrounds', async(req, res) => {
 app.get('/campgrounds/:id', async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
+})
+
+// Edit
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+})
+
+// Upgrade - put 메소드
+app.put('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 
 app.listen(3000, () => {
