40d93072bf61fe5b13482e30b088d5a085923906
Griotold <gotjd9773@naver.com>
2023-01-22 11:01:02 +0900

    ejs-mate를 활용한 layout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const uri = 'mongodb://127.0.0.1:27017/yelp-camp';
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -17,6 +18,8 @@ db.once("open", () => {
 })
     
 const app = express();
+
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
