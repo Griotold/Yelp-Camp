acdf55478100c597494de904f4e041676e08e27d
Griotold <gotjd9773@naver.com>
2023-02-02 11:10:25 +0900

    Joi + 오류 핸들링 마무리

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const uri = 'mongodb://127.0.0.1:27017/yelp-camp';
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const {campgroundSchema} = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -28,6 +30,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+    
+}
+
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -42,12 +55,12 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 // Create -> 44. 에러 핸들링 추가
-app.post('/campgrounds', catchAsync(async(req, res, next) => {
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+app.post('/campgrounds', validateCampground, catchAsync(async(req, res, next) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-    
     
 }))
 
@@ -64,7 +77,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 }))
 
 // Upgrade - put 메소드
-app.put('/campgrounds/:id', catchAsync(async(req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
