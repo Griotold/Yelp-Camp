8417ec5eb34b22816c42f47ae3b031bd8eca680a
Griotold <gotjd9773@naver.com>
2023-02-21 10:59:42 +0900

    flash_error Partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -39,12 +39,20 @@ router.post('/', validateCampground, catchAsync(async(req, res, next) => {
 // Show
 router.get('/:id', catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show', { campground });
 }))
 
 // Edit
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/edit', {campground});
 }))
 
