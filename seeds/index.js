11961ab540d56033ffae841b7ebc9e3d3854b6a8
Griotold <gotjd9773@naver.com>
2023-02-28 18:16:16 +0900

    518. campground에 저자 추가

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '63fdc3521d476e50e247f361',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'http://source.unsplash.com/collection/483251',
