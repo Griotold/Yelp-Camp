11dea0f92b71654a7b24695b2c5fd82d786a9b0b
Griotold <gotjd9773@naver.com>
2023-03-02 11:53:15 +0900

    526. router.route() : 경로 설정을 위한 멋진 방법

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,23 +6,17 @@ const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 const Campground = require('../models/campground');
 
-router.get('/', catchAsync(campgrounds.index));
-// New -> Show 보다 위에 있어야 함.
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-// Create -> 44. 에러 핸들링 추가
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-// Show
-router.get('/:id', catchAsync(campgrounds.showCampground));
-
-// Edit
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-// Upgrade - put 메소드
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-// Delete
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
