55670dde68f9e06f45a53266af88d53c78bc4b09
Griotold <gotjd9773@naver.com>
2023-03-08 13:36:28 +0900

    547 맵에 마커 표시 완성

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,10 +2,10 @@ mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: lngLat, // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 7, // starting zoom
 });
 
 new mapboxgl.Marker()
-    .setLngLat(lngLat)
+    .setLngLat(campground.geometry.coordinates)
     .addTo(map);
