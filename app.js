a229b0859ca48b2f097e8a71584a4d1202c20b2b
Griotold <gotjd9773@naver.com>
2023-03-13 11:58:56 +0900

    566 XSS 공격 막기

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-if(process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-
-
 
 const express = require('express');
 const path = require('path');
@@ -17,41 +15,44 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-})
-    
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
 const app = express();
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
-    secret : 'thisshouldbebettersecret!',
-    resave : false,
-    saveUninitialized : true,
+    secret: 'thisshouldbebettersecret!',
+    resave: false,
+    saveUninitialized: true,
     cookie: {
         httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
 app.use(session(sessionConfig));
 app.use(flash());
 
@@ -63,20 +64,18 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.session);
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
-})
+});
 
 app.get('/fakeUser', async (req, res) => {
-    const user = new User(
-        { email : 'coltttt@gmail.com',
-            username: 'coltttt'});
+    const user = new User({ email: 'coltttt@gmail.com', username: 'coltttt' });
     const newUser = await User.register(user, 'chicken');
     res.send(newUser);
-})
+});
 
 app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
@@ -84,22 +83,20 @@ app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
-})
-
+});
 
 // 모든 라우트에 대한 콜백. 순서가 맨 아래에 있어야 함.
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
-})
+});
 
 // Error Handling -> Section 42. 앱의 오류 처리하기 참고
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if(!err.message) err.message = 'Oh No, Something Went Wrong!';
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
     res.status(statusCode).render('error', { err });
-})
-
+});
 
 app.listen(3000, () => {
-    console.log("Serving on port 3000");
-})
+    console.log('Serving on port 3000');
+});
